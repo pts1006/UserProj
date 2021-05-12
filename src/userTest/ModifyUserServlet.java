d9f7e8627005cf6488a67d4ce1a165731161aaf5
admin <admin@YD02-13>
2021-05-12 09:37:45 +0900

    210512 09:37

--- src/userTest/ModifyUserServlet.java
+++ src/userTest/ModifyUserServlet.java
@@ -7,10 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONObject;
-
-
-
 @WebServlet("/modifyUserServlet")
 public class ModifyUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
--- src/userTest/UserDAO.java
+++ src/userTest/UserDAO.java
@@ -7,8 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import userTest.DBCon;
-
 public class UserDAO {
 
 	Connection conn;
